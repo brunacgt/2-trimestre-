895e4c3c2e14edc700bff03b496b39cad324d076
brunacgt <117641824+brunacgt@users.noreply.github.com>
2024-05-28 08:45:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
+const perguntas = [
+  {
+    enunciado:"você nasceu em Curitiba?";
+    alternativas: [
+      "sim",
+      "não",
+  ]
+},                                            
+  {
+    enunciado:"você gosta de frio?";
+    alternativas: [
+      "sim",
+      "não",
+  ]
+},                                            
+  {
+    enunciado:"qual sua estação do ano favorita?";
+    alternativas: [
+      "inverno",
+      "verão",
+  ]
+},                                            
+  {
+    enunciado:"você moraria no alaska?";
+    alternativas: [
+      "Alternativa1",
+      "Alternativa2",
+  ]
+},                                            
+  {
+    enunciado:"você moraria no deserto?";
+    alternativas: [
+      "Alternativa1",
+      "Alternativa2",
+  ]
+},                                            
+];
